bac2ef6317f5aa25fb7d82c9f05cc62b4ca532ab
Dave Loyall <dave@the-good-guys.net>
2016-01-06 17:33:00 -0600

    improvements

--- blitter.lisp
+++ blitter.lisp
@@ -6,28 +6,26 @@
                      :direction :IO
                      :if-does-not-exist :ERROR
 		     :element-type '(unsigned-byte 8))
-  (loop for y from -1 to 1 by 0.1
-     do (loop for x from -1 to 1 by 0.1
-	   do (if (equal y x)
+  (loop for y from -1 to 0.8 by 0.01
+     do (loop for x from -1 to 0.8 by 0.01
+	   do (if T
 		  (progn 
 		    (print "x=") (princ x)
 		    (print "y=") (princ y)
 		    (print "coords=") (princ (float-cart-coords x y))
-		    ;(file-position fb0 (coords x y))
-		    ;(write-byte #x10 fb0)
-		    ;(write-byte #xFF fb0)
-		    ;(write-byte #x10 fb0)
-		    ))) 
-       ))
-
+		    (file-position fb0 (float-cart-coords x y))
+		    (write-byte #x10 fb0)
+		    (write-byte #xFF fb0)
+		    (write-byte #x10 fb0))))))
+q
 (defun wipe (r g b)
   (with-open-file (fb0 "/dev/fb0"
 		       :direction :IO
 		       :if-does-not-exist :ERROR
 		       :element-type '(unsigned-byte 8))
     (file-position fb0 :start)
-    (loop for i from 1 to (* 640 480)
-       do (write-byte b fb0)
+    (loop for i from 1 to (* 640 480) do
+	 (write-byte b fb0)
 	 (write-byte g fb0)
 	 (write-byte r fb0))))
 
@@ -39,17 +37,20 @@
 
 
 (defun coords (x y)
-  (+ (* x 3) (* 640 3 y)))
+  (round (* 3 (+ x (* 640 y)))))
 
 (defun cart-coords (x y)
   (coords (+ 320 x) (+ 240 y)))
 
 (defun float-cart-coords (x y)
-  (cart-coords (* 320) (* 240)))
+  (cart-coords (* 320 x) (* 240 y)))
 
-(define-test test-cart-coords
-    (let* ((letters '(:a :b :c :d))
-           (loop-result
-             (loop for letter in letters
-                   collect letter)))
-      (assert-equal loop-result '(:a :b :c :d))))
+
+(float-cart-coords 1.0 -0.8 )
+
+;; (define-test letters-loop
+;;     (let* ((letters '(:a :b :c :d))
+;;            (loop-result
+;; 	      (loop for letter in letters
+;; 		 collect letter)))
+;;       (assert-equal loop-result '(:a :b :c :d))))

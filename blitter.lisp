86b49c07f2984f564389732313befe8b538ebff8
Dave Loyall <dave@the-good-guys.net>
2016-01-08 12:59:17 -0600

    using a circle function for the fade, reorg

--- blitter.lisp
+++ blitter.lisp
@@ -2,35 +2,29 @@
 
 (in-package #:blitter)
 
-(with-open-file (fb0 "/dev/fb0"
-                     :direction :IO
-                     :if-does-not-exist :create
-		     :if-exists :supersede
-		     :element-type '(unsigned-byte 8))
-  (loop for y from 0 to 479 by 1 do 
-       (loop for x from 0 to 639 by 1 do
-	    
-	  ;; (print "x=") (princ x)
-	  ;; (print "y=") (princ y)
-	  ;; (print "coords=") (princ (float-cart-coords x y))
-	    (file-position fb0 (coords x y))
-	    (write-byte #x10 fb0)
-	    (if (and
-		 (>=
-		  (+ (second (coords-cart-float x y)) 0.05)
-		  (sin (first (coords-cart-float x y))))
-		 (<=
-		  (- (second (coords-cart-float x y)) 0.05)
-		  (sin (first (coords-cart-float x y)))))
-		(write-byte #xFF fb0)
-		(write-byte #x10 fb0))
-	    (write-byte #x10 fb0))))
+(defun coords (x y)
+  (round (* 3 (+ x (* 640 y)))))
 
-(with-open-file (fb0 "~/scratch/sine4.data"
+(defun coords-cart-float (x y)
+  (list (/ (- 320 x) -32) (/ (- 240 y) -32)))
+
+(defun radius-fall (x)
+  (let ((n
+	 (- (expt 255 2) (* (expt x 2)850000))))
+    (if (>= n 0)
+	(round (sqrt n))
+	0)))
+
+(with-open-file (fb0
+		 ;"/dev/fb0" ;;;This is the linux framebuffer, basically your screen's memory.
+                             ;;; If you write directly to it, what is displayed on your screen
+		             ;;; changes immediately.
+		 "~/scratch/sine5.data" ;;;Gimp can open raw bitmap files. This one is RGB
+		                        ;;; data, 8 bits each.
                      :direction :IO
                      :if-does-not-exist :create
 		     :if-exists :supersede
-		     :element-type '(unsigned-byte 8))
+		     :element-type '(unsigned-byte 8)) ; 8 bits, we read or write a byte at a time.
   (loop for y from 0 to 479 by 1 do 
        (loop for x from 0 to 639 by 1 do
 	    
@@ -43,10 +37,20 @@
 					   (sin (first (coords-cart-float x y)))
 					   (second (coords-cart-float x y))))
 				     ) fb0)
-	    (write-byte #x10 fb0))))
+	    (write-byte #x10 fb0)))
+  )
+
+;;;an alternative radius-fall function.
+(defun radius-fall (x)
+  (round (* (+
+	     (atan (+ 70  (*
+			   (expt x 0.1)
+			   -200)))
+	     (/ pi 2))
+	    (/ 255 pi))))
 
 
-
+;;Here's a function to clear the screen.
 (defun wipe (r g b)
   (with-open-file (fb0 "/dev/fb0"
 		       :direction :IO
@@ -58,77 +62,55 @@
 	 (write-byte g fb0)
 	 (write-byte r fb0))))
 
-(wipe #x90 #x90 #x90)
+;;Call it like this:
+;(wipe #x90 #x90 #x90)
 
 
-(with-open-file (outfile "~/scratch/coords.txt"
-			 :direction :output
-			 :if-exists :supersede
-			 :if-does-not-exist :create)
-  (loop for y from 0 to 479 do
-       (loop for x from 0 to 639 do
-	    (format outfile "~d from (coords ~d ~d)~%" (coords x y) x y))))
 
-(with-open-file (outfile "~/scratch/cart-coords.txt"
-			 :direction :output
-			 :if-exists :supersede
-			 :if-does-not-exist :create)
-  (loop for y from -240 to 239 do
-       (loop for x from -320 to 319 do
-	    (format outfile "~d from (cart-coords ~d ~d)~%" (cart-coords x y) x y))))
+;;;testing...
+;(list (radius-fall 10) (radius-fall 1) (radius-fall 0.5) (radius-fall 0.1) (radius-fall 0.00000001))
+;;(0 0 166 252 255)
+;;(0 0 165.9066 252.04166 255.0)
 
-(with-open-file (outfile "~/scratch/float-cart-coords.txt"
-			 :direction :output
-			 :if-exists :supersede
-			 :if-does-not-exist :create)
-  (loop for y from 0.98 to 0.998 by 0.0035 do
-       (loop for x from 0.98 to 0.998 by 0.0035 do 
-	    (format outfile "~A from (float-cart-coords ~d ~d)~%" (float-cart-coords x y) x y))))
 
-(defun radius-fall (x)
-  (round (* (+
-	     (atan (+ 35  (*
-			   (expt x 0.8)
-			   -20)))
-	     (/ pi 2))
-	    (/ 255 pi))))
+;;;I don't think these two are needed anymore!
+;; (defun cart-coords (x y)
+;;   (coords (+ 320 x) (+ 240 y)))
 
-(list (radius-fall 10)      (radius-fall 1)  (radius-fall 0.5)    (radius-fall 0.1)      (radius-fall 0.00000001))
-(1 250 252 252 253)
-(3 250 251 252 253)
-(0 250 252 253 253)
-(0 247 251 252 252)
-(0 1 7 251 252)
-(0 1 251 252)
-(0 0 2 252)
-(0 0 1 247)
+;; (defun float-cart-coords (x y)
+;;   (cart-coords (round (* 320 x)) (round (* 240 y))))
 
-(0 0 247 247)
-(0 1 246 247)
 
-(0 127 220 229)
-(0 35 127 128)
-(0 98 143 157)
-
-(defun coords (x y)
-  (round (* 3 (+ x (* 640 y)))))
-
-(defun cart-coords (x y)
-  (coords (+ 320 x) (+ 240 y)))
-
-(defun float-cart-coords (x y)
-  (cart-coords (round (* 320 x)) (round (* 240 y))))
-
-(defun coords-cart-float (x y)
-  (list (/ (- 320 x) -32) (/ (- 240 y) -32)))
-
-(coords-cart-float 0 0)
-
-(float-cart-coords 1.0 -0.8 )
-
+;;; Todo: figure out how to write tests like this one:
 ;; (define-test letters-loop
 ;;     (let* ((letters '(:a :b :c :d))
 ;;            (loop-result
 ;; 	      (loop for letter in letters
 ;; 		 collect letter)))
 ;;       (assert-equal loop-result '(:a :b :c :d))))
+
+;;; Alternative sine-drawing function, doesn't use radius-fall and has some different
+;;; structure in the if.
+;; (with-open-file (fb0 "/dev/fb0"
+;;                      :direction :IO
+;;                      :if-does-not-exist :create
+;; 		     :if-exists :supersede
+;; 		     :element-type '(unsigned-byte 8))
+;;   (loop for y from 0 to 479 by 1 do 
+;;        (loop for x from 0 to 639 by 1 do
+	    
+;; 	  ;; (print "x=") (princ x)
+;; 	  ;; (print "y=") (princ y)
+;; 	  ;; (print "coords=") (princ (float-cart-coords x y))
+;; 	    (file-position fb0 (coords x y))
+;; 	    (write-byte #x10 fb0)
+;; 	    (if (and
+;; 		 (>=
+;; 		  (+ (second (coords-cart-float x y)) 0.05)
+;; 		  (sin (first (coords-cart-float x y))))
+;; 		 (<=
+;; 		  (- (second (coords-cart-float x y)) 0.05)
+;; 		  (sin (first (coords-cart-float x y)))))
+;; 		(write-byte #xFF fb0)
+;; 		(write-byte #x10 fb0))
+;; 	    (write-byte #x10 fb0))))

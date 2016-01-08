ff05fe583227058c306f06e89ed0fc029908eeed
Dave Loyall <dave@the-good-guys.net>
2016-01-07 19:09:45 -0600

    not boxy!

--- blitter.lisp
+++ blitter.lisp
@@ -2,22 +2,50 @@
 
 (in-package #:blitter)
 
-(with-open-file (fb0 "~/scratch/sine.raw"
+(with-open-file (fb0 "/dev/fb0"
                      :direction :IO
                      :if-does-not-exist :create
 		     :if-exists :supersede
 		     :element-type '(unsigned-byte 8))
-  (loop for y from -1 to 0.998 by 0.0035 do 
-       (loop for x from -1 to 0.998 by 0.0035 do
-	    (if (and (>= (+ y 0.001) (sin x)) (<= (- y 0.001) (sin x)))
-		(progn 
-		  ;; (print "x=") (princ x)
-		  ;; (print "y=") (princ y)
-		  ;; (print "coords=") (princ (float-cart-coords x y))
-		  (file-position fb0 (float-cart-coords x y))
-		  (write-byte #x10 fb0)
-		  (write-byte #xFF fb0)
-		  (write-byte #x10 fb0))))))
+  (loop for y from 0 to 479 by 1 do 
+       (loop for x from 0 to 639 by 1 do
+	    
+	  ;; (print "x=") (princ x)
+	  ;; (print "y=") (princ y)
+	  ;; (print "coords=") (princ (float-cart-coords x y))
+	    (file-position fb0 (coords x y))
+	    (write-byte #x10 fb0)
+	    (if (and
+		 (>=
+		  (+ (second (coords-cart-float x y)) 0.05)
+		  (sin (first (coords-cart-float x y))))
+		 (<=
+		  (- (second (coords-cart-float x y)) 0.05)
+		  (sin (first (coords-cart-float x y)))))
+		(write-byte #xFF fb0)
+		(write-byte #x10 fb0))
+	    (write-byte #x10 fb0))))
+
+(with-open-file (fb0 "~/scratch/sine4.data"
+                     :direction :IO
+                     :if-does-not-exist :create
+		     :if-exists :supersede
+		     :element-type '(unsigned-byte 8))
+  (loop for y from 0 to 479 by 1 do 
+       (loop for x from 0 to 639 by 1 do
+	    
+	  ;; (print "x=") (princ x)
+	  ;; (print "y=") (princ y)
+	  ;; (print "coords=") (princ (float-cart-coords x y))
+	    (file-position fb0 (coords x y))
+	    (write-byte #x10 fb0)
+	    (write-byte (radius-fall (abs (-
+					   (sin (first (coords-cart-float x y)))
+					   (second (coords-cart-float x y))))
+				     ) fb0)
+	    (write-byte #x10 fb0))))
+
+
 
 (defun wipe (r g b)
   (with-open-file (fb0 "/dev/fb0"
@@ -30,11 +58,8 @@
 	 (write-byte g fb0)
 	 (write-byte r fb0))))
 
-(loop for i from 1 to 10
-   do
-     (wipe #x00 #x00 #x00)
-     (wipe #x50 #x50 #x50)
-     )
+(wipe #x90 #x90 #x90)
+
 
 (with-open-file (outfile "~/scratch/coords.txt"
 			 :direction :output
@@ -60,6 +85,30 @@
        (loop for x from 0.98 to 0.998 by 0.0035 do 
 	    (format outfile "~A from (float-cart-coords ~d ~d)~%" (float-cart-coords x y) x y))))
 
+(defun radius-fall (x)
+  (round (* (+
+	     (atan (+ 35  (*
+			   (expt x 0.8)
+			   -20)))
+	     (/ pi 2))
+	    (/ 255 pi))))
+
+(list (radius-fall 10)      (radius-fall 1)  (radius-fall 0.5)    (radius-fall 0.1)      (radius-fall 0.00000001))
+(1 250 252 252 253)
+(3 250 251 252 253)
+(0 250 252 253 253)
+(0 247 251 252 252)
+(0 1 7 251 252)
+(0 1 251 252)
+(0 0 2 252)
+(0 0 1 247)
+
+(0 0 247 247)
+(0 1 246 247)
+
+(0 127 220 229)
+(0 35 127 128)
+(0 98 143 157)
 
 (defun coords (x y)
   (round (* 3 (+ x (* 640 y)))))
@@ -70,6 +119,10 @@
 (defun float-cart-coords (x y)
   (cart-coords (round (* 320 x)) (round (* 240 y))))
 
+(defun coords-cart-float (x y)
+  (list (/ (- 320 x) -32) (/ (- 240 y) -32)))
+
+(coords-cart-float 0 0)
 
 (float-cart-coords 1.0 -0.8 )
 

f2c275d9431b6b475e5e8a1aaca6bd8d80cffc10
Dave Loyall <dave@the-good-guys.net>
2016-01-16 09:34:37 -0600

    WIP

--- blitter.lisp
+++ blitter.lisp
@@ -119,3 +119,101 @@
 ;; 		(write-byte #xFF fb0)
 ;; 		(write-byte #x10 fb0))
 ;; 	    (write-byte #x10 fb0))))
+
+(defun draw-line ;( x1 y1 x2 y2 )
+   (xy1 xy2)
+  (let ((x1 (round (first  xy1)))
+	(y1 (round (second xy1)))
+	(x2 (round (first  xy2)))
+	(y2 (round (second xy2))))
+    
+;  (declare (type rgb-pixel-buffer buffer))
+  (declare (type integer x1 y1 x2 y2))
+;  (declare (type rgb-pixel pixel))
+  (let* ((dist-x (abs (- x1 x2)))
+         (dist-y (abs (- y1 y2)))
+         (steep (> dist-y dist-x)))
+    (when steep
+      (psetf x1 y1 y1 x1
+             x2 y2 y2 x2))
+    (when (> x1 x2)
+      (psetf x1 x2 x2 x1
+             y1 y2 y2 y1))
+    (let* ((delta-x (- x2 x1))
+           (delta-y (abs (- y1 y2)))
+           (error (floor delta-x 2))
+           (y-step (if (< y1 y2) 1 -1))
+           (y y1))
+      (loop 
+        :for x :upfrom x1 :to x2
+        :do (if steep 
+                (paint x y #x00 #x00 #x00)
+                (paint y x #x00 #x00 #x00))
+            (setf error (- error delta-y))
+            (when (< error 0)
+              (incf y y-step)
+              (incf error delta-x)))))))
+
+
+(defun paint (x y red green blue)
+  (with-open-file (fb0 "/dev/fb0"
+		       :direction :IO
+		       :if-does-not-exist :ERROR
+		       :element-type '(unsigned-byte 8))
+    (file-position fb0 (coords x y))
+    (write-byte blue fb0)
+    (write-byte green fb0)
+    (write-byte red fb0)))
+
+
+(dotimes (x 100)
+  (paint x x #xFF #x00 #x00))
+
+(defun draw-rectangle (xy1 xy2)
+  (let ((x1 (first  xy1))
+	(y1 (second xy1))
+	(x2 (first  xy2))
+	(y2 (second xy2)))
+    
+    (draw-line (list x1 y1) (list x1 y2))
+    (draw-line (list x1 y2) (list x2 y2))
+    (draw-line (list x2 y2) (list x2 y1))
+    (draw-line (list x2 y1) (list x1 y1))))
+
+
+(defun draw-rectangular-prism (x1 y1 z1 x2 y2 z2)
+  (let ((corner1 (ortho-project x1 y1 z1))
+	(corner2 (ortho-project x2 y1 z1))
+	(corner3 (ortho-project x1 y2 z1))
+	(corner4 (ortho-project x2 y2 z1))
+	(corner5 (ortho-project x1 y1 z2))
+	(corner6 (ortho-project x2 y1 z2))
+	(corner7 (ortho-project x1 y2 z2))
+	(corner8 (ortho-project x2 y2 z2)))
+    (draw-rectangle corner1 corner4)
+    (draw-rectangle corner5 corner8)
+    (draw-line corner1 corner5)
+    (draw-line corner2 corner6)
+    (draw-line corner3 corner7)
+    (draw-line corner4 corner8)))
+
+(defun ortho-project (x y z)
+  (list
+   (+ x (* 0.5 z))				;x
+   (+ y (* 0.5 z))))					;y
+
+
+
+
+(progn (draw-rectangular-prism 10 10 10 100 100 100)
+       (draw-rectangular-prism 14 14 14 96 96 96)
+       (draw-rectangular-prism 18 18 18 92 92 92)
+       (draw-rectangular-prism 22 22 22 88 88 88))
+
+(draw-rectangle '(10 10) '(50 50))
+
+(draw-line 10 10 30 50)
+
+(font-a ())
+
+
